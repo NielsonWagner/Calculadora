daca9f6b93b95e6eee4e80b24f94f685836075c4
Nielson Wagner <nilwp@hotmail.com>
2016-05-27 00:22:02 -0300

    Comit Escola

---
+++ src/calculadora/Main.java
@@ -0,0 +1,17 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String [] args){
+        String nome = lerString();
+        Calculadora calc = new Calculadora(nome);
+        
+        boolean vaiPraProximo   = true;
+        
+        Scanner leitor = new Scanner (System.in);
+    
+    }
+
+    
+}
